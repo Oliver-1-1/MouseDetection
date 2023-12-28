b34be1efaf660c258ed62e3c7842d02838919709
Oliver <66334947+Oliver-1-1@users.noreply.github.com>
2023-12-28 21:17:10 +0100

    Update main.cpp

--- MouseMove/main.cpp
+++ MouseMove/main.cpp
@@ -3,7 +3,9 @@
 
 //https://github.com/ekknod/MouseClassServiceCallbackTrick also gets caught
 //https://github.com/ekknod/MouseClassServiceCallbackMeme 
-//Hardware(kmbox etc) prob dosnt get caught, never tested never will test :D
+//NtInjectedMouseInput gets caught
+//mouse_event gets caught. was some weird here tho with admin etc.
+//Hardware(kmbox etc) prob doesn't get caught, never tested never will test :D
 
 void main() {
 	//Move mouse with software every 500 ms.
